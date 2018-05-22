34b9e4133868b2ba1cc3ce9867a63f8f50d568a7
YOKOTATE-PC\yokotate <yokosekazuya@yahoo.co.jp>
2018-05-22 22:56:22 +0900

    PIL change to cv2. AND add test data.

---
+++ code/datasetCreate_cv2.py
@@ -0,0 +1,106 @@
+# 画像をnpz形式に変換する
+# Create NPZ file
+import numpy as np
+from PIL import Image
+import os, glob, random, re, cv2
+
+# 変数設定
+photo_size = 92 # 画像サイズ
+
+def glob_images(path, label, max_photo, rotate):
+    used_file = {}
+    files = glob.glob(path + "/*.pgm")
+    # 取り出した画像の順番をランダムに変更する
+    random.shuffle(files)
+    i = 0
+    for f in files:
+        if i >= max_photo:break
+        if f in used_file:continue
+        used_file[f] = True
+        i += 1
+        img = cv2.imread(f, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        x.append(image_to_data(img))
+        y.append(label)
+        
+        if not rotate: continue
+        # 以下テストデータ水増し用画像編集部分
+        average(img, label)
+        contrast(img, label)
+
+
+# 平滑化
+def average(img, label):
+    for average in range(2, 6, 1):
+        average_square = (average, average)
+        blur_img = cv2.blur(img, average_square)
+        x.append(image_to_data(img))
+        y.append(label)
+        average_square = (1, average)
+        blur_img = cv2.blur(img, average_square)
+        x.append(image_to_data(img))
+        y.append(label)
+        average_square = (average, 1)
+        blur_img = cv2.blur(img, average_square)
+        x.append(image_to_data(img))
+        y.append(label)    # ルックアップテーブルの生成
+
+# コントラスト調整
+def contrast(img, label):
+    # 以下データの水増し用画像変換
+    min_table = 50
+    max_table = 205
+    diff_table = max_table - min_table
+
+    LUT_HC = np.arange(256, dtype = 'uint8' )
+    LUT_LC = np.arange(256, dtype = 'uint8' )
+
+    # ハイコントラストLUT作成
+    for i in range(0, min_table):
+        LUT_HC[i] = 0
+    for i in range(min_table, max_table):
+        LUT_HC[i] = 255 * (i - min_table) / diff_table
+    for i in range(max_table, 255):
+        LUT_HC[i] = 255
+
+    # ローコントラストLUT作成
+    for i in range(256):
+        LUT_LC[i] = min_table + i * (diff_table) / 255
+    
+    high_cont_img = cv2.LUT(img, LUT_HC)
+    x.append(image_to_data(high_cont_img))
+    y.append(label)
+    low_cont_img = cv2.LUT(img, LUT_LC)
+    x.append(image_to_data(low_cont_img))
+    y.append(label)
+
+    # 平滑化
+    average(high_cont_img, label)
+    average(low_cont_img, label)
+
+def image_to_data(img): # 画像データを正規化
+  data = np.asarray(img)
+  data = img / 256
+  data = data.reshape(92, 112, 1) # RGBなら3を指定する
+  return data
+
+def make_dataset(max_photo, outfile, rotate):
+    global x
+    global y
+    x = []
+    y = []
+
+    # folder_path = "./att_faces/"
+    folders = glob.glob("./att_faces/*")
+    for folder in folders:
+        glob_images(folder,int(os.path.basename(folder)[1:]) - 1,max_photo,rotate)
+    x = np.array(x, dtype=np.float32)
+    np.savez(outfile, x=x, y=y)
+    print("saved:" + outfile)
+
+print("Create Start!!")
+make_dataset(12000, "photo.npz", rotate=True)
+print("Success crate LearningData!")
+make_dataset(1000, "photo-test.npz", rotate=False)
+print("Success create TestData!")
+
---
+++ code/learning_cv2.py
@@ -0,0 +1,95 @@
+import numpy as np
+import glob
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,Dropout,Flatten,Dense
+from keras.callbacks import TensorBoard
+
+data = np.load("./photo.npz")
+x_train = data["x"]
+y_train = data["y"]
+data = np.load("./photo-test.npz")
+x_test = data["x"]
+y_test = data["y"]
+classes = len(glob.glob("./att_faces/*"))
+
+x_train = x_train / 255.
+x_test = x_test / 255.
+
+y_train = to_categorical(y_train, classes)
+y_test = to_categorical(y_test, classes)
+
+model = Sequential()
+
+
+model.add(
+    Conv2D(
+        filters=92,
+        input_shape=(92,112,1),
+        kernel_size=(5,5),
+        strides=(2,2),
+        padding='same',
+        activation='relu'
+    )
+)
+model.add(
+    Conv2D(
+        filters=92,
+        kernel_size=(5,5),
+        strides=(2,2),
+        padding='same',
+        activation='relu'
+    )
+)
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(
+    Conv2D(
+        filters=184,
+        kernel_size=(5,5),
+        strides=(2,2),
+        padding='same',
+        activation='relu'
+    )
+)
+model.add(
+    Conv2D(
+        filters=184,
+        kernel_size=(5,5),
+        strides=(2,2),
+        padding='same',
+        activation='relu'
+    )
+)
+
+model.add(MaxPooling2D(pool_size=(2,2), dim_ordering="th"))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.output_shape
+
+model.add(Dense(units=512, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(units=classes, activation='softmax'))
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+tsb=TensorBoard(log_dir='./logs')
+history_model = model.fit(
+    x_train,
+    y_train,
+    batch_size=100,
+    epochs=20,
+    validation_split=0.5,
+    callbacks=[tsb]
+)
+model.save_weights("nn.hdf5")
+score = model.evaluate(x_test, y_test)
+print('loss = ', score[0])
+print('accuracy = ', score[1])
+
